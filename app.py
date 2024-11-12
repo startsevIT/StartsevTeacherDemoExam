3cc76de570cc5947d3d6a7560b05ab9b790084f9
Кирилл <s.t.a.r.t.s.e.v.it@gmail.com>
2024-11-12 15:19:31 +0500

    Исправлено форматирование сообщения

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def update_order(dto : Annotated[UpdateOrderDTO, Form()]):
         if o.number == dto.number:
             if dto.status != o.status and dto.status != "":
                 o.status = dto.status
-                message += "Статус заявки №{o.number} изменен"
+                message += f"Статус заявки №{o.number} изменен\n"
             if dto.description != "":
                 o.description = dto.description
             if dto.master != "":
