51429c3b727e9f3c43ee962e489a0c79630407ec
Кирилл <s.t.a.r.t.s.e.v.it@gmail.com>
2024-11-15 16:46:49 +0500

    Добавлен функционал уведомлений о завершении и добавления комментариев

--- app.py
+++ app.py
@@ -13,12 +13,14 @@ class Order(BaseModel):
     client : str
     status : str
     master : Optional[str] = "Не назначен"
+    comments : Optional[str] = []
 
 class UpdateOrderDTO(BaseModel):
     number: int
     status: Optional[str] = ""
     description: Optional[str] = ""
     master: Optional[str] = ""
+    comment : Optional[str] = str
 
 repo = [
     Order(
@@ -82,9 +84,13 @@ def update_order(dto : Annotated[UpdateOrderDTO, Form()]):
             if dto.status != o.status and dto.status != "":
                 o.status = dto.status
                 message += f"Статус заявки №{o.number} изменен\n"
+                if(o.status == "выполнено"):
+                    message += f"Заявка №{o.number} завершена\n"
             if dto.description != "":
                 o.description = dto.description
             if dto.master != "":
                 o.master = dto.master
+            if dto.comment != None and dto.comment != "":
+                o.comments.append(dto.comment)
             return o
     return "Не найдено"
