8e80dbcf3aa62c365ff37b30e0851cf2444cf9eb
Кирилл <s.t.a.r.t.s.e.v.it@gmail.com>
2024-11-20 10:34:54 +0500

    Реализован пункт 2.5 полностью

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime 
 from typing import Annotated, Optional
 from pydantic import BaseModel
 from fastapi import FastAPI, Form
@@ -12,8 +12,9 @@ class Order(BaseModel):
     description : str
     client : str
     status : str
+    endDate: Optional[datetime.date] = None
     master : Optional[str] = "Не назначен"
-    comments : Optional[str] = []
+    comments : Optional[list] = []
 
 class UpdateOrderDTO(BaseModel):
     number: int
@@ -86,6 +87,7 @@ def update_order(dto : Annotated[UpdateOrderDTO, Form()]):
                 message += f"Статус заявки №{o.number} изменен\n"
                 if(o.status == "выполнено"):
                     message += f"Заявка №{o.number} завершена\n"
+                    o.endDate = datetime.datetime.now()
             if dto.description != "":
                 o.description = dto.description
             if dto.master != "":
@@ -94,3 +96,31 @@ def update_order(dto : Annotated[UpdateOrderDTO, Form()]):
                 o.comments.append(dto.comment)
             return o
     return "Не найдено"
+
+def complete_count():
+    return len([o for o in repo if o.status == "выполнено"])
+
+def get_problem_type_stat():
+    dict = {}
+    for o in repo:
+        if o.problemType in dict.keys():
+            dict[o.problemType] += 1
+        else:
+            dict[o.problemType] = 1
+    return dict
+
+def get_average_time_to_complete():
+    times = [(
+        datetime.datetime.fromisoformat(o.endDate.isoformat()) -
+        datetime.datetime.fromisoformat(o.startDate.isoformat())).days 
+                 for o in repo 
+                 if o.status == "выполнено"]
+    if complete_count() != 0:
+        return sum(times) / complete_count()
+    return 0
+    
+@app.get("/statistics")
+def get_statistics():
+    return {"complete_count" : complete_count(),
+        "problem_type_stat" : get_problem_type_stat(),
+        "average_time_to_complete" : get_average_time_to_complete()}
